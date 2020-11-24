cb5b320730f97789f728d6b2dee43bcc7cf7fd2b
Hector Lopez <hectorvmlopez@gmail.com>
2020-11-24 11:25:14 -0500

    my first testing in react using react, and enzyme

---
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -0,0 +1,24 @@
+//Unit Testing
+import React from 'react';
+
+import {configure, shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+//import components
+import NavigationItems from './NavigationItems';
+import NavigatiomItem from './NavigationItem/NavigationItem';
+
+//CONNECT ENZYME
+configure({adapter: new Adapter(),});
+
+
+
+describe('<NavigationItems /> Test', () => {
+
+    //write actual testing
+    it('should render Two <NavigatiomItem/> elements if not authenticated', () => {
+        //actual unit testing logic
+        const wrapper =  shallow(<NavigationItems />);
+        expect(wrapper.find(NavigatiomItem)).toHaveLength(2);
+    })
+    
+})
