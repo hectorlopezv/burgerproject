ec6aa2d5e06a7bea35b08fe25c1ff04a01f22488
Hector Lopez <hectorvmlopez@gmail.com>
2020-11-24 20:19:20 -0500

    adding all method from saga to yield al methods

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,21 +1,19 @@
 //listen to certain acttions and do something when they occur
-import {takeEvery} from 'redux-saga/effects';
+import {takeEvery, all} from 'redux-saga/effects';
 import * as actionTypes from '../actions/actionTypes';
 import {logoutSaga, checkAuthTimeoutSaga, authSaga, authCheckState } from './auth';
 import {initIngridients} from './burgerbuilder';
 import {purchaseBurgerSaga, fetchOrdersSaga} from './order';
 export function* watchAuth(){
     //setting a listener to this action and then execute saga
-    yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga);
-    yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga);
-    yield takeEvery(actionTypes.AUTH_USER, authSaga);
-    yield takeEvery(actionTypes.AUTH_CHECK_STATE, authCheckState);
-    yield takeEvery(actionTypes.INIT_INGRIDIENT, initIngridients);
-    
-    yield takeEvery(actionTypes.PURCHASE_BURGER, purchaseBurgerSaga);
-    yield takeEvery(actionTypes.FETCH_ORDER_INIT, fetchOrdersSaga);
-
-
-
+    yield all([
+    takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga),
+    takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga),
+    takeEvery(actionTypes.AUTH_USER, authSaga),
+    takeEvery(actionTypes.AUTH_CHECK_STATE, authCheckState),
+    takeEvery(actionTypes.INIT_INGRIDIENT, initIngridients),
+    takeEvery(actionTypes.PURCHASE_BURGER, purchaseBurgerSaga),
+    takeEvery(actionTypes.FETCH_ORDER_INIT, fetchOrdersSaga),
+]);
 }
 
