77e29e4f17b47cf51b81aade248aebfdb4f12b7f
Hector Vinicio Lopez Molinares <hectorvmlopez@gmail.com>
2020-11-27 18:50:04 -0600

    passing class components to hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,  { Component} from 'react';
+import React,  { Component, useEffect} from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -29,48 +29,41 @@ const asynAuth = asyncComponent(()=>{
   return import('./containers/Auth/Auth')
 });
 
+const App = (props) => {
+  useEffect(() => {
+    props.onTryAutoSignup()
+    console.log('entro');
+  }, [])
 
+  let routes = (
+    <Switch>
+    <Route path="/auth" component={asynAuth}/>
+    <Route exact path="/" component={BurgerBuilder}/>
+    <Redirect to="/"/>
+    </Switch>
+);
 
+ if (props.isAuthenticated) {
 
+   routes = (
+     <Switch>
+       <Route path="/checkout" component={asynCheckout}/>
+       <Route path="/orders" component={asynOrder}/>
+       <Route path="/logout" component={Logout}/>
+       <Route path="/auth" component={asynAuth}/>
+       <Route exact path="/" component={BurgerBuilder}/> 
+       <Redirect to="/"/>   
+     </Switch>
+   );
+ }
 
-
-
-class App extends Component {
-  componentDidMount() {
-    this.props.onTryAutoSignup()
-  }
-  render(){
-
-    let routes = (
-       <Switch>
-        <Route path="/auth" component={asynAuth}/>
-        <Route exact path="/" component={BurgerBuilder}/>
-        <Redirect to="/"/>
-       </Switch>
-    );
-
-    if (this.props.isAuthenticated) {
-
-      routes = (
-
-        <Switch>
-          <Route path="/checkout" component={asynCheckout}/>
-          <Route path="/orders" component={asynOrder}/>
-          <Route path="/logout" component={Logout}/>
-          <Route path="/auth" component={asynAuth}/>
-          <Route exact path="/" component={BurgerBuilder}/> 
-          <Redirect to="/"/>   
-        </Switch>
-
-      );
-    }
-    return (
-            <Layout> 
-              {routes}
-            </Layout>
-    );
-  }
+  return ( 
+    <Layout> 
+      {routes}
+    </Layout>
+   );
 }
+ 
 
 
 
